c7168508b3e6c468e917c67bee7744faa3288086
Carson Strout <carson.strout42@gmail.com>
2022-09-30 11:42:27 -0600

    Add empty project

---
+++ Individual Game/Assets/Scripts/CoinCollect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+    public PlatformManager platformManagerScript;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (ScoreCounter.coinAmount < 10)
+        {
+            ScoreCounter.coinAmount += 1; // increases number of coins collected
+            platformManagerScript.NewPlatform(); // calls method to move collectible to next spot
+        }
+    }
+}
---
+++ Individual Game/Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    // Array setup for platforms
+    GameObject[] platforms;
+    GameObject currentPlatform;
+    int index = 0;
+
+    // GameObject for coin appearing above platform
+    public GameObject coin;
+
+    private void Start()
+    {
+        NewPlatform();
+    }
+    // Start is called before the first frame update
+    public void NewPlatform()
+    {
+        platforms = GameObject.FindGameObjectsWithTag("Platform"); // Creates an array of all objects with the tag platform
+        currentPlatform = platforms[index]; // registers random platform as the one the player must get to
+        if (index < 11)
+        {
+            index++;
+            coin.transform.position = new Vector2(currentPlatform.transform.position.x, currentPlatform.transform.position.y); // moves to next platform
+        }
+    }
+}
